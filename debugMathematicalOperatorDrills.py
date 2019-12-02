d85fcf4267b98f5051b71e3dddb103d70b7c8ea7
wolfalejandro <57291114+wolfalejandro@users.noreply.github.com>
2019-12-01 23:31:55 -0600

    Add files via upload

---
+++ debugMathematicalOperatorDrills.py
@@ -0,0 +1,78 @@
+'''
+For this assignment you should look at the operations done below and find what
+the answer to the operation would be.
+EXAMPLE TASK:
+'''
+#EX)
+#x = 4
+#y = 5
+#answer = x + y
+#answer is:
+9
+'''
+END EXAMPLE
+'''
+
+'''
+START HERE
+'''
+#1)
+#x = 4
+#y = 4
+#answer = x + y
+#answer is:
+8
+#2)
+#x = 4
+#y = 5
+#answer = x - y
+#answer is:
+-1
+#3)
+#x = 4
+#y = 5
+#answer = x * y
+#answer is:
+20
+#4)
+#x = 4
+#y = 5
+#answer = x / y
+#answer is:
+0.8
+#5)
+#x = 10
+#y = 5
+#answer = x % y
+#answer is:
+0
+#6)
+#x = 10
+#y = 4
+#answer = x // y
+#answer is:
+2
+#7)
+#x = 4
+#y = 5
+#answer = x ** y
+#answer is:
+1024
+#8)
+#x = "stuff"
+#y = "things"
+#answer = x + y
+#answer is:
+stuffthings
+#9)
+#x = "stuff "
+#y = " and things"
+#answer = x + y
+#answer is:
+stuff and things
+#10)
+#x = "you "
+#y = "are done"
+#answer = x + y
+#answer is:
+you are done
