e01402e899b423ea79fe0ee03137f9c3798d4506
wolfalejandro <57291114+wolfalejandro@users.noreply.github.com>
2019-12-01 23:24:33 -0600

    Add files via upload

---
+++ mathematicalOperatorDrills.py
@@ -0,0 +1,180 @@
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a variable called num and assign it to 4 plus 5. Then print the variable.
+num = 4 + 5
+print(num)
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+'''
+ADDITION
+'''
+#1) Make a variable called math and assign it to 4 plus 5. Then print the variable.
+math= 4+5
+print(math)
+
+#2) Make a variable called q and assign it to 6 plus 10. Then print the variable.
+q = 6 + 10
+print(q)
+
+#3) Make a variable called w and assign it to math plus q. Then print the variable.
+w = math + q
+print(w)
+#4) Make a variable called string and assign it to "concat" plus " is fun". 
+#   Then print the variable.
+
+string = “concat” + “is fun”
+print(string)
+
+#5) Make a variable called first and assign it to your first name. 
+#   Make a variable called last and assign it to your last name.
+#   Make a variable called e and assign it to first + last.
+#   Then print the variable.
+
+first = “alejandro”
+last = “wolf”
+e = first + last
+SUBTRACTION
+'''
+
+#1) Make a variable called r and assign it to 4 minus 5. Then print the variable.
+r = 4 - 5
+print(r)
+#2) Make a variable called t and assign it to 6 minus 10. Then print the variable.
+t = 6 - 10
+print(t)
+#3) Make a variable called y and assign it to t minus r. Then print the variable.
+y = t - r
+print(y)
+'''
+MULTIPLICATION
+'''
+
+#1) Make a variable called u and assign it to 4 times 5. Then print the variable.
+u = 4 * 5
+print(u)
+#2) Make a variable called i and assign it to 5 times 3. Then print the variable.
+i = 5 * 3
+print(i)
+#3) Make a variable called o and assign it to i times u. Then print the variable.
+o = i * u
+print(o)
+#4) Make a variable called string and assign it to "string" times 3. Then print the variable.
+string= “string” * 3
+print(string)
+'''
+DIVISION
+'''
+
+#1) Make a variable called p and assign it to 4 divided by 5. Then print the variable.
+p = 4 / 5
+print(p)
+#2) Make a variable called a and assign it to 5 divided by 3. Then print the variable.
+a= 5 / 3
+print(a)
+#3) Make a variable called s and assign it to a divided by p. Then print the variable.
+s= a / p
+print(s)
+'''
+MODULO
+'''
+
+#1) Make a variable called d and assign it to 5 modulo 3. Then print the variable.
+d = 5 % 3
+print(d)
+#2) Make a variable called f and assign it to 234 modulo 3. Then print the variable.
+f = 234 % 3
+print(f)
+#3) Make a variable called g and assign it to f modulo d. Then print the variable.
+g = f % d
+print(g)
+'''
+
+
+
+
+FLOOR DIVISION
+
+
+'''
+
+
+
+
+
+
+
+#1) Make a variable called h and assign it to 5 floor divided by 3. Then print the variable.
+
+
+h = 5 // 3
+
+
+print(h)
+
+
+#2) Make a variable called j and assign it to 234 floor divided by 3. Then print the variable.
+
+
+j = 234//3
+
+
+print(j)
+
+
+#3) Make a variable called k and assign it to h floor divided by j. Then print the variable.
+
+
+k= h//j
+
+
+print(k)
+
+
+'''
+
+
+EXPONENTIATION
+
+
+'''
+
+
+
+
+
+
+
+#1) Make a variable called l and assign it to 2 to 2nd power. Then print the variable.
+
+
+l= 2**2
+
+
+print(l)
+
+
+#2) Make a variable called z and assign it to 3 to the 3rd power. Then print the variable.
+
+
+z= 3**3
+
+
+print(z)
+
+
+#3) Make a variable called x and assign it to l to the z power. Then print the variable.
+
+
+x= l**z
+
+
+print(x)
